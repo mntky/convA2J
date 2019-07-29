e7d5ba6ee2b8370c44eccd6b5a1e7b4c532f1876
minato <47530912+mntky@users.noreply.github.com>
2019-07-29 14:19:11 +0900

    Update conv.go

--- conv.go
+++ conv.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	testdata := "token=vFlRfdwGp9Nk5koVtSaaaa&team_id=teamaaaa&team_domain=xxxwork-club&service_id=65176051510&channel_id=JSJ7T8PM&channel_name=test&timestamp=1559196502400&user_id=43K5KD0&user_name=mntky&text=ojisan&trigger_word=ojisan"
+	testdata := "token=vFlRfdwGp9Nk5Saaaa&team_id=teamaaaa&team_domain=xxxwork-test&service_id=65176050000&channel_id=JSJ7TTTT4&channel_name=test&timestamp=1500096502400&user_id=43K5GGG&user_name=mntky&text=ojisan&trigger_word=ojisan"
 
 	var key []string
 	var data []string
